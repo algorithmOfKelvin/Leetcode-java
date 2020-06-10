308ff358da912f44bc0f85963c8e5fce69207cc6
xianfan <549652152@qq.com>
2020-06-11 05:06:25 +0800

    update .gitignore

--- src/main/java/vip/kelvin/leetCode/C739_dailyTemperatures.java
+++ src/main/java/vip/kelvin/leetCode/C739_dailyTemperatures.java
@@ -1,8 +1,34 @@
 package vip.kelvin.leetCode;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author Kelvin范显
  * @date 2020/6/11 上午4:51
  */
 public class C739_dailyTemperatures {
+    public int[] dailyTemperatures(int[] T) {
+        int n = T.length;
+        int[] res = new int[n];
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            int t = T[i];
+            while (!stack.isEmpty() && t > T[stack.peek()]) {
+                int prevIndex = stack.pop();
+                res[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        C739_dailyTemperatures s = new C739_dailyTemperatures();
+        int[] r = s.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
+        System.out.println(JSONObject.toJSON(r));
+    }
 }
